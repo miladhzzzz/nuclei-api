2bd3f3d651fa8d658e810cb11ad499d6bc3a82be
milx <david.hacker09@gmail.com>
2025-03-06 18:21:19 +0330

    Fix: Typo in helpers package name

--- app/helpers/config.py
+++ app/helpers/config.py
@@ -6,6 +6,9 @@ class Config():
     def __init__(self) -> None:
         load_dotenv()
     
+    nuclei_upload_template_path = os.getenv("NUCLEI_CUSTOM_TEMPLATE_UPLOAD_PATH")
+    nuclei_template_path = os.getenv("NUCLEI_TEMPLATE_PATH")
+    app_port = os.getenv("APP_PORT")
     sentry_dsn = os.getenv("SENTRY_DSN")
     env = os.getenv("ENVIRONMENT")
     release = os.getenv("RELEASE")
